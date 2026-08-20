b41b2c715edcdf72d1a8f25f5345327020bf28ff
Marcos Silva <1609.marcos.silva@gmail.com>
2026-08-20 11:36:19 -0300

    Teste de experiencia do usuario

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -8,7 +8,7 @@ import type {
 
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://staymetricsia.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
